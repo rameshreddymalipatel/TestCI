bb1cf3d1d885260f2bea345241158b521bae6003
Ramesh Reddy <ramesh0067@gmail.com>
2017-01-16 23:53:11 +0530

    Junit

---
+++ src/test/JunitTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import citool.TestCI;
+
+public class JunitTest {
+		
+
+		@Test
+		public void testSample() {
+			TestCI test = new TestCI();
+			Assert.assertEquals(test.sample().toString(),"sample");
+		}
+
+	
+}
