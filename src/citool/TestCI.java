c1ae2c619ecd815e45d0f50f57f467bf352187f5
Ramesh Reddy <ramesh0067@gmail.com>
2017-01-16 14:39:48 +0530

    Adding Exception

--- src/citool/TestCI.java
+++ src/citool/TestCI.java
@@ -3,6 +3,8 @@ package citool;
 public class TestCI {
 public static void main(String args[]){
 	System.out.println("Test CI Success");
+	int a =10/0;
+	System.out.println(a);
 	
 	System.out.println("Yes");
 	
