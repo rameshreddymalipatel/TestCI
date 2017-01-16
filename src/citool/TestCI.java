5ea36a715db06bc7490bc1ad4c6e3265da52e9f3
Ramesh Reddy <ramesh0067@gmail.com>
2017-01-16 14:42:17 +0530

    build force fail

--- src/citool/TestCI.java
+++ src/citool/TestCI.java
@@ -3,10 +3,7 @@ package citool;
 public class TestCI {
 public static void main(String args[]){
 	System.out.println("Test CI Success");
-	int a =10/0;
-	System.out.println(a);
-	
-	System.out.println("Yes");
+	System.out.println("Yes")
 	
 }
 	
