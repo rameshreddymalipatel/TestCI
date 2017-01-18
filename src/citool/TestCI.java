94d18081d740b0e4247abb5548716135e7f66dab
Ramesh Reddy <ramesh0067@gmail.com>
2017-01-18 11:18:06 +0530

    mesaage added

--- src/citool/TestCI.java
+++ src/citool/TestCI.java
@@ -4,6 +4,7 @@ public class TestCI {
 	public static void main(String args[]) {
 		System.out.println("Test CI Success");
 		System.out.println("Yes");
+		System.out.println("Jenkins Build is Successful");
 	}
 
 	public String sample() {
